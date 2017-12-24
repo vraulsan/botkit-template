e5b8136f4da538abe3ccc25fcdc3a47ff231c3d2
Victor <vraulsan@gmail.com>
2017-12-24 03:24:37 -0500

    asd

--- bot.js
+++ bot.js
@@ -113,23 +113,6 @@ controller.setupWebserver(port, function (err, webserver) {
     console.log("Cisco Spark: healthcheck available at: " + process.env.HEALTHCHECK_ROUTE);
 });
 
-var imap = {
-  user: "vraulsan@gmail.com",
-  password: process.env.MAILERPASSWORD,
-  host: "imap.gmail.com",
-  port: 993, //imap port
-  tls: true,
-  tlsOptions: { rejectUnauthorized: false }
-};
-
-notifier(imap)
-  .on('mail', mail => console.log(mail))
-  .start();
-const n = notifier(imap);
-n.on('end', () =>  n.start())
-  .on('mail', mail => console.log(mail.from[0].address, mail.subject))
-  .start();
-
 
 
 //
