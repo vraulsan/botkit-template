ab76baac5c693bbbec42eb4d596c86f3ae914b4a
Victor <vraulsan@gmail.com>
2017-12-24 03:22:45 -0500

    asd

--- skills/mailer.js
+++ skills/mailer.js
@@ -1,9 +1,58 @@
+const EWS = require('node-ews');
+const fs = require('fs');
 
+// exchange server connection info
+const ewsConfig = {
+  username: 'victor.sanchezmaggiolo@charter.com',
+  password: 'C!scoJuniper192182',
+  host: 'https://outlook.charter.com'
+};
 
-/*
+const ews = new EWS(ewsConfig);
+
+// specify listener service options
+const serviceOptions = {
+  port: 8000, // defaults to port 8000
+  path: '/', // defaults to '/notification'
+  // If you do not have NotificationService.wsdl it can be found via a quick Google search
+  xml: fs.readFileSync('../NotificationService.wsdl', 'utf8') // the xml field is required
+};
+
 
 module.exports = function(controller) {
   controller.hears('mailer', 'direct_message,direct_mention', function(bot, message) {
+    ews.notificationService(serviceOptions, function(response) {
+      console.log(new Date().toISOString(), '| Received EWS Push Notification');
+      console.log(new Date().toISOString(), '| Response:', JSON.stringify(response));
+      // Do something with response
+      return {SendNotificationResult: { SubscriptionStatus: 'OK' } }; // respond with 'OK' to keep subscription alive
+      // return {SendNotificationResult: { SubscriptionStatus: 'UNSUBSCRIBE' } }; // respond with 'UNSUBSCRIBE' to unsubscribe
+    });
+
+    const ewsArgs = {
+      PushSubscriptionRequest: {
+        FolderIds: {
+          DistinguishedFolderId: {
+            attributes: {
+              Id: 'inbox'
+            }
+          }
+        },
+        EventTypes: {
+          EventType: ['CreatedEvent']
+        },
+        StatusFrequency: 1,
+        // subscription notifications will be sent to our listener service
+        URL: 'https://spark-piebot.herokuapp.com:8000/'
+      }
+    };
+    ews.run('Subscribe', ewsArgs)
+    .then(result => {
+      console.log(prettyjson.render(result));
+    })
+    .catch(err => {
+      console.log(err.message);
+    });
+
     bot.reply(message, 'yoyo ' + name);
   });
-*/
