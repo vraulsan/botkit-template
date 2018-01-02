ad4044793854975d02c5fe1f0244d05c7f1612fa
Victor <vraulsan@gmail.com>
2018-01-01 21:34:33 -0500

    edit

--- skills/ticketer.js
+++ skills/ticketer.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 
-const url = "https://23782374.ngrok.io/tickets"
+const url = "https://87fa2c4b.ngrok.io/tickets"
 
 module.exports = function(controller) {
   controller.hears('tickets', 'direct_message,direct_mention', function(bot, message) {
@@ -10,7 +10,7 @@ module.exports = function(controller) {
         //console.log(typeof res.data)
         finalText = generateTicketList(res.data)
         //console.log(finalText);
-        bot.reply(message, {text: 'If you had markdown...', markdown: finalText})
+        bot.reply(message, finalText)
       })
       .catch(err => { console.log(err) })
     bot.reply(message, 'One sec, let me fetch that real quick...');
@@ -21,19 +21,19 @@ var generateTicketList = results => {
   var text = ''
   var ticketCount = 0;
   var ticketUna = 0;
-  console.log('HERE FULL RESULTS: ', results)
+  //console.log('HERE FULL RESULTS: ', results)
   Object.keys(results).forEach( key => {
     var topKey = results[key];
     ticketCount = ticketCount + 1
     if (topKey['Assignee Name'] == null) {
         ticketUna = ticketUna + 1
+        topKey['Assignee Name'] = 'Unassigned'
       }
     Object.keys(topKey).forEach( key => {
-      text = text +'**'+key + ':** ' + topKey[key] + '\n\n'
+      text = text + key + ': ' + topKey[key] + '\n'
     })
-    text = text + '- - -' + '\n\n'
+    text = text + '-----------------------------------------------------------' + '\n'
   })
-  text = text + '## Total Tickets: ' + ticketCount + '\n\n' + '## Total Unassigned: ' + ticketUna;
-  console.log(text);
+  text = '```\n' + text + '```\n### Total Tickets: ' + ticketCount + '\n\n' + '### Total Unassigned: ' + ticketUna;
   return text
 }
